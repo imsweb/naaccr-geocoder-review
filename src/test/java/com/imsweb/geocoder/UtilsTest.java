1d8aefa91c404196110bfbd70de894447ce76029
schadega <SchadeggA@imsweb.com>
2017-08-02 13:24:11 -0400

    Replaced sample file

--- src/test/java/com/imsweb/geocoder/UtilsTest.java
+++ src/test/java/com/imsweb/geocoder/UtilsTest.java
@@ -20,7 +20,7 @@ public class UtilsTest {
 
     @Test
     public void testParseHeaders() throws IOException {
-        URL url = Thread.currentThread().getContextClassLoader().getResource("sample_input_csv.csv");
+        URL url = Thread.currentThread().getContextClassLoader().getResource("sample_input_c.csv");
         Assert.assertNotNull(url);
         Assert.assertEquals(139, Utils.parseHeaders(new File(url.getFile())).size());
         // TODO check a few other headers, don't need to be too exhaustive...
@@ -28,7 +28,7 @@ public class UtilsTest {
 
     @Test
     public void testParserJsonFields() throws IOException {
-        URL url = Thread.currentThread().getContextClassLoader().getResource("sample_input_csv.csv");
+        URL url = Thread.currentThread().getContextClassLoader().getResource("sample_input_c.csv");
         Assert.assertNotNull(url);
         Assert.assertEquals(76, Utils.parserJsonFields(new File(url.getFile())).size());
         // TODO assert a bit more stuff
@@ -38,7 +38,7 @@ public class UtilsTest {
     public void testMapJsonFieldsToHeaders() throws IOException {
         // TODO I put this test quickly to make sure the method was working, but I don't think the test should use the file; it shouldn't call the other parse method.
         // TODO so it should be changed to build fake list of data and use those instead of the file.
-        URL url = Thread.currentThread().getContextClassLoader().getResource("sample_input_csv.csv");
+        URL url = Thread.currentThread().getContextClassLoader().getResource("sample_input_c.csv");
         Assert.assertNotNull(url);
         Map<String, String> mappings = Utils.mapJsonFieldsToHeaders(Utils.parserJsonFields(new File(url.getFile())), Utils.parseHeaders(new File(url.getFile())));
         //for (Map.Entry<String, String> mapping : mappings.entrySet())
